fc40e84c304a22cf9237e85f7ca0b2ac34dcf0a4
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-03-29 16:28:25 +0200

    Added Loading Spinner

---
+++ frontend/src/components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FiCommand } from "react-icons/fi";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <FiCommand className="loading-icon" />
+    </div>
+  );
+};
+
+export default Loader;
