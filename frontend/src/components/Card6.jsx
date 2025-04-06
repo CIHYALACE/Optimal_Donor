8895f0bf2226b49a1b8849aca59392703f9acd61
AbdelRahmna <abodyoussef2015@gmail.com>
2025-04-06 15:31:52 +0200

    Refactor Card6 component layout and styling for improved structure and readability

--- frontend/src/components/Card6.jsx
+++ frontend/src/components/Card6.jsx
@@ -4,18 +4,20 @@ import ProgressBar from "./ProgressBar";
 export default function Card6({title, text, imgSrc, totalAmount, currentAmount}) {
   return (
     <>
-      <div className="card bg-inherit border-0 flex-md-nowrap text-white m-0 col-12 col-md-4">
+      <div className="card bg-inherit border-0 flex-md-nowrap text-white m-md-0 mb-3 col-12 col-md-4">
         <img src={imgSrc} className="card-img" alt="..." />
-        <div className="card-img-overlay">
+        <div className="card-img-overlay d-flex flex-column justify-content-end align-items-start">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text mb-5">{text}</p>
-          <ProgressBar
-            currentAmount={currentAmount}
-            totalAmount={totalAmount}
-          />
-            <p className="card-text">
-                <small>${currentAmount}/{totalAmount}</small>
-            </p>
+          <p className="d-none d-md-block card-text">{text}</p>
+          <div className="w-75">
+            <ProgressBar
+              currentAmount={currentAmount}
+              totalAmount={totalAmount}
+            />
+          </div>
+          <p className="card-text">
+              <small>${currentAmount}/${totalAmount}</small>
+          </p>
         </div>
       </div>
     </>
