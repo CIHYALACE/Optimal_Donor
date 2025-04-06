3282449c3382ab73e1a094250ebfdcbad33699e8
hosam <semryhosam@gmail.com>
2025-04-06 13:46:57 +0200

    Update header styling to match main theme

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <Navbar expand="lg" className="bg-body-tertiary position-sticky top-0 z-1">
       <Container>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Brand href="\" className="font-weight-bold fs-md-3 fw-bold big-shoulders">
+        <Navbar.Brand href="\" className="font-weight-bold fs-md-3 fw-bold big-shoulders d-flex align-items-center flex-row gap-2">
           <img
             alt=""
             src="/donate.png"
@@ -16,7 +16,7 @@ export default function Header() {
             height="45"
             className="d-inline-block align-top"
           />{" "}
-          Optimal Donor
+          <span className='fw-bold fs-4 text-success '>Optimal Donor</span>
         </Navbar.Brand>
         <NavLink className={"nav-link"}><i className="fa-solid fa-search fs-5 d-sm-inline d-lg-none px-2"></i></NavLink>
         <NavLink className={"nav-link"} to="/login"><i className="fa-solid fa-user fs-5 d-sm-inline d-lg-none px-2"></i></NavLink>
@@ -26,8 +26,8 @@ export default function Header() {
             <Nav.Link className="nav-link" href="#link">About</Nav.Link>
             <Nav.Link className={"nav-link"} href="/campaigns">Campaigns</Nav.Link>
             <Nav.Link className="nav-link" href="#link">Contact</Nav.Link>
-            <NavLink className={"nav-link"}><i className="fa-solid fa-search fs-5 d-none d-lg-inline px-2"></i></NavLink>
-            <NavLink className={"nav-link"} to="/login"><i className="fa-solid fa-user fs-5 d-none d-lg-inline px-2"></i></NavLink>
+            <NavLink className={"nav-link"}><i className="fa-solid fa-search fs-5 d-none d-lg-inline px-2 text-success"></i></NavLink>
+            <NavLink className={"nav-link"} to="/login"><i className="fa-solid fa-user fs-5 d-none d-lg-inline px-2 text-success"></i></NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
