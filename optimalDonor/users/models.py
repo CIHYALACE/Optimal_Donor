e1e1485666e35801dc1c3eeaff45b6e5365d3025
hosam <semryhosam@gmail.com>
2025-04-07 19:46:49 +0200

    Modified user model

--- optimalDonor/users/models.py
+++ optimalDonor/users/models.py
@@ -1,33 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
-# from core.models import Campaign
+from django.apps import apps
+...
+
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    name = models.CharField(max_length=255, blank=False, null=False)
     phone_number = models.CharField(blank=True, null=True, unique=True, max_length=11,
-                    validators=[RegexValidator(
-                                        regex=r'^01\d{9}$',
-                                        message="Phone number must be 11 digits and start with '01'.",
-                                        code='invalid_phone_number'
-                                    )
-                                ])
-    picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    username = None
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']# if u wanna add more ,do
-
+    validators=[RegexValidator(
+    regex=r'^01\d{9}$',
+    message="Phone number must be 11 digits and start with '01'.",
+    code='invalid_phone_number'
+    )
+    ])
     def __str__(self):
-        return self.email
-    # return self.username
+        return self.username
 
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
+    picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def get_donated_campaigns(self):
+        Campaign = apps.get_model('core', 'Campaign')
+        return Campaign.objects.filter(donations__user=self)
 
     def __str__(self):
-        return self.user.email
+        return self.user.username
--- optimalDonor/users/serializers.py
+++ optimalDonor/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import *
+from .models import CustomUser, UserProfile
+from core.models import Campaign
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -8,21 +9,45 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'name', 'phone_number', 'picture', 'password']
+        fields = ['id', 'email', 'username', 'phone_number', 'password']
 
+    def create(self, validated_data):
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            phone_number=validated_data.get('phone_number')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+    
 
 class UserProfileSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     user = CustomUserSerializer(required=False)
+    donated_campaigns = serializers.SerializerMethodField()
 
     class Meta:
         model = UserProfile
         fields = ["id", "user", "bio", "location", "date_of_birth", "picture", "donated_campaigns"]
 
+    def get_donated_campaigns(self, obj):
+        campaigns = obj.get_donated_campaigns().distinct()
+        return CampaignSerializer(campaigns, many=True).data
+
     def update(self, instance, validated_data):
         user_data = validated_data.pop('user', None)
         if user_data:
             user_serializer = CustomUserSerializer(instance.user, data=user_data, partial=True)
-            if user_serializer.is_valid():
-                user_serializer.save()
+            user_serializer.is_valid(raise_exception=True)
+            user_serializer.save()
         return super().update(instance, validated_data)
