eaeb28e9726d1e736cfad70d46373288a66d7690
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-04-08 21:35:09 +0200

    Add CustomUserAdmin

--- optimalDonor/users/admin.py
+++ optimalDonor/users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    model = CustomUser
+
+    list_display = ('email', 'name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+
+    ordering = ('email',)
+    search_fields = ('email', 'name')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name', 'phone_number', 'picture')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
