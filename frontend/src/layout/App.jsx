913fcd84e7517133f0c2e1e464fd26e31c2f4159
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-04-14 07:19:13 +0200

    Add activate, reset, and confirm Routers

--- frontend/src/layout/App.jsx
+++ frontend/src/layout/App.jsx
@@ -9,7 +9,9 @@ import CampaignsPage from "../pages/CampaignsPage";
 import CampaignDetailsPage from "../pages/CampaignDetailsPage";
 import AboutPage from "../pages/About";
 import CampaignCreationPage from "../pages/CampaignCreationPage";
-
+import Activate from "../pages/Activate";
+import ResetPassword from "../pages/ResetPassword";
+import ResetPasswordConfirm from "../pages/ResetPasswordConfirm";
 
 function App() {
   return (
@@ -18,10 +20,17 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/profile/" element={<UserProfile/>} />
+        <Route path="/activate/:uid/:token" element={<Activate />} />
+        <Route path="/users/activate/:uid/:token" element={<Activate />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route
+          path="/password/reset/confirm/:uid/:token"
+          element={<ResetPasswordConfirm />}
+        />
+        <Route path="/profile/" element={<UserProfile />} />
         <Route path="/campaigns" element={<CampaignsPage />} />
         <Route path="/campaigns/:id" element={<CampaignDetailsPage />} />
-        <Route path="/campaigns/create" element={<CampaignCreationPage/>}/>
+        <Route path="/campaigns/create" element={<CampaignCreationPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<h1>404</h1>} />
       </Route>
