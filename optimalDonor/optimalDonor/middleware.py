37391b3f3503da7f135e8791782408ab5c5da5b7
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-04-15 20:10:36 +0200

    Stop using force domain middleware ; causes static files issues

--- optimalDonor/optimalDonor/middleware.py
+++ optimalDonor/optimalDonor/middleware.py
@@ -1,5 +1,8 @@
-from django.utils.deprecation import MiddlewareMixin
+"""from django.utils.deprecation import MiddlewareMixin
 
 class ForceDomainMiddleware(MiddlewareMixin):
     def process_request(self, request):
         request.META['HTTP_HOST'] = 'localhost:5173'
+
+"""
+# adding this middleware causes some static media issues
