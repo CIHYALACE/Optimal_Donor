1b4606d2460b4d1392aca5081a0da346978969fc
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-04-15 20:33:58 +0200

    Add MEDIA_ROOT path

--- optimalDonor/optimalDonor/settings.py
+++ optimalDonor/optimalDonor/settings.py
@@ -197,4 +197,4 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 # Media files settings
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
