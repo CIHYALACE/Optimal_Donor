7c5106e8aba8f798223b8a2fc0e2afc99461f8ae
mohmdloai <mhmd.00.eng@gmail.com>
2025-09-29 21:12:38 +0300

    Update: settings

--- optimalDonor/optimalDonor/settings.py
+++ optimalDonor/optimalDonor/settings.py
@@ -117,13 +117,13 @@ DATABASES = {
 
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv('EMAIL_SERVER')
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = f"Optimal Donor <{os.getenv('EMAIL_HOST_USER')}>"
+DEFAULT_FROM_EMAIL = f"Optimal Donor <{os.getenv('DEFAULT_FROM_EMAIL')}>"
 
 # Optimal donor domain settings
 SITE_DOMAIN = "localhost:5173"  # Update to the frontend's domain and port
